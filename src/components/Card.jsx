df4dad4bd9ec236371d46f74f2f01935fbf71b0e
Alexandre <xand974@gmail.com>
2021-07-05 12:11:29 +0200

    ajout anim

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,24 +5,26 @@ export default function Card({ imgSource, link }) {
     var canMove = true;
 
     function HandleMouseMove(e){
-        canMove = true;
 
         let X = Math.floor(e.clientX / 20);
-        let Y = Math.floor(e.clientY / 50);
+        let Y = Math.floor(e.clientY / 2);
 
-        const animText = document.getElementById('animText');
+        const texts = document.querySelectorAll('.text');
 
-        animText.style.top = `${X}px`;
-        animText.style.right = `${Y}px`;
-        
+        for(let i = 0 ; i <texts.length ; i++){
+            texts[i].style.transform = `translateX(${X}px)`;
+            texts[i].style.transform = `translateY(${Y}px)`;
+        }
+         
+     
     }
 
 
   return (
     <div className="card">
       <img src={imgSource} alt="" className="card__img" />
-      <div onMouseMove={canMove && HandleMouseMove} onMouseLeave={canMove = false} className="card__link">
-        <a href={link} id="animText" className="text">
+      <div onMouseMove={canMove && HandleMouseMove} onMouseEnter={canMove = true} onMouseLeave={canMove = false} className="card__link">
+      <a href={link} id="animText" className="text">
           Read
         </a>
       </div>
