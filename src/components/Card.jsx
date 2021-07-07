613715c2d21c9fca364a8fee147a0fd1a080afde
Alexandre <xand974@gmail.com>
2021-07-07 13:16:28 +0200

    fix target style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import "./card.css"
+import "./card.css";
 
 export default function Card({ imgSource, link }) {
-    var canMove = true;
+  var canMove = true;
 
-    function HandleMouseMove(e){
+  function HandleMouseMove(e) {
+    console.log(e.currentTarget.style);
+    let X = Math.floor(e.clientX / 30);
+    let Y = Math.floor(e.clientY / 30);
 
-        let X = Math.floor(e.clientX / 20);
-        let Y = Math.floor(e.clientY / 2);
-
-        const texts = document.querySelectorAll('.text');
-
-        for(let i = 0 ; i <texts.length ; i++){
-            texts[i].style.transform = `translateX(${X}px)`;
-            texts[i].style.transform = `translateY(${Y}px)`;
-        }
-         
-     
-    }
-
+    e.currentTarget.style.transform = `translateX(${X}px)`;
+    e.currentTarget.style.transform = `translateY(${Y}px)`;
+  }
 
   return (
     <div className="card">
       <img src={imgSource} alt="" className="card__img" />
-      <div onMouseMove={canMove && HandleMouseMove} onMouseEnter={canMove = true} onMouseLeave={canMove = false} className="card__link">
-      <a href={link} id="animText" className="text">
+      <div className="card__link">
+        <a
+          onMouseMove={canMove && HandleMouseMove}
+          onMouseEnter={(canMove = true)}
+          onMouseLeave={(canMove = false)}
+          href={link}
+          id="animText"
+          className="text"
+        >
           Read
         </a>
       </div>
